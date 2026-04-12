8736a7b779ef2cbdd90b463b0b3f3f76651ade5c
Kenan <duvnjakkenan3@gmail.com>
2026-04-12 18:04:52 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-﻿namespace OOP_Projekt
+namespace OOP_Projekt
 {
+
     using System;
 
     // Startpunkt för programmet och menyhantering
@@ -13,6 +14,8 @@
             // Loop som visar meny tills användaren avslutar
             while (running)
             {
+                Console.Clear();
+
                 Console.WriteLine("\n1. Insert Money");
                 Console.WriteLine("2. Show Products");
                 Console.WriteLine("3. Buy Product");
@@ -26,32 +29,48 @@
                 if (!int.TryParse(input, out choice))
                     continue;
 
+                // Från knap 1-5
                 switch (choice)
                 {
                     case 1:
+                        Console.WriteLine("Mata in hur mycket pengar du vill lägga in (5, 10, 20, 50):");
                         int money;
                         if (int.TryParse(Console.ReadLine(), out money))
                             vm.InsertMoney(money);
+                        else
+                            Console.WriteLine("Felaktig inmatning!");
+                            Console.ReadKey();
                         break;
 
                     case 2:
                         vm.ShowProducts();
+                        Console.ReadKey();
                         break;
 
                     case 3:
+                        vm.ShowProducts();
+                        Console.WriteLine("Välj produkt genom att ange numret:");
                         int productChoice;
                         if (int.TryParse(Console.ReadLine(), out productChoice))
                             vm.Purchase(productChoice);
+                        else
+                            Console.WriteLine("Felaktigt val!");
+                            Console.ReadKey();
                         break;
 
                     case 4:
+                        Console.WriteLine("Avslutar köp och returnerar pengar...");
                         vm.EndTransaction();
+                        Console.ReadKey();
                         break;
 
                     case 5:
+                        Console.WriteLine("Avslutar programmet...");
                         vm.EndTransaction();
                         running = false;
+                        Console.ReadKey();
                         break;
+      
                 }
             }
         }
